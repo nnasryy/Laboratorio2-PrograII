9252fb95fa09c8e911febe6b4a107448bcbc1cf7
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-01-30 08:59:23 -0600

    Clase Empleado

--- Lab2_PrograII/src/lab2_prograii/Empleado.java
+++ Lab2_PrograII/src/lab2_prograii/Empleado.java
@@ -4,10 +4,44 @@
  */
 package lab2_prograii;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author nasry
  */
-public class Empleado {
+public abstract class Empleado {
+    
+    //Atributos
+    protected String codigo;
+    protected String nombre;
+    protected Date fechaContratacion;
+    protected double salarioBase;
+    protected int horasTrabajadas;
+    protected String rutaFoto;
+
+    //Atributos
+    public Empleado(String codigo, String nombre, Date fecha, double salario, int horasTrabajadas, String foto) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.fechaContratacion = fecha;
+        this.salarioBase = salario;
+        this.horasTrabajadas = 0;
+        this.rutaFoto = foto;
+    }
+    
+    //setter
+    public void registrarHoras(int horas){
+        this.horasTrabajadas=horas;
+    }
+    
+    public abstract double calcularPago();
+    
+    public String mostarInfo(){
+        return "ID: " + codigo + " | Nombre: " + nombre + " | Fecha: " + 
+                new SimpleDateFormat("dd/MM/yyyy").format(fechaContratacion);
+    }
+    
     
 }
